1840b32249967b3e8e954ca4c62ec0c97461109f
Tom Buchan <tbuchan1997@gmail.com>
2024-10-31 11:30:04 +0000

    add js functionality

---
+++ script.js
@@ -0,0 +1,39 @@
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+generateJoke();
+
+//Using async/await
+
+async function generateJoke() {
+    const config = {
+        headers: {
+            Accept: "application/json"
+        }
+    }
+    const res = await fetch("https://icanhazdadjoke.com/", 
+        config)
+
+        const data = await res.json();
+        jokeEl.innerHTML = data.joke;
+    
+}
+
+jokeBtn.addEventListener("click", generateJoke);
+
+//Using .then()
+
+//function generateJoke() {
+//    const config = {
+//        headers: {
+//            Accept: "application/json"
+//        }
+//    }
+//    fetch("https://icanhazdadjoke.com/", config)
+//    .then(res => res.json())
+//    .then((data) => {
+//        jokeEl.innerHTML = data.joke;
+//    })
+//}
+
+//jokeBtn.addEventListener("click", generateJoke);
